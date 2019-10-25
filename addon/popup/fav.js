bcd8e0bc1c1ff8afcca289570dd31f85dc83191d
cheong <cheongjw1998@gmail.com>
2019-10-25 12:49:24 +0800

    favourite-ui-change

--- addon/popup/fav.js
+++ addon/popup/fav.js
@@ -8,83 +8,83 @@ var data = {
 }
 
 if (typeof(Storage) !== "undefined") {
-    // Code for localStorage/sessionStorage.
+  // Code for localStorage/sessionStorage.
 
-    //set data object with value or empty object in storage
-    data = localStorage.getItem('favData') ? JSON.parse(localStorage.getItem(`favData`)) : data
+  //set data object with value or empty object in storage
+  data = localStorage.getItem('favData') ? JSON.parse(localStorage.getItem(`favData`)) : data
 
-    //show data in favorite tab
-    showFav(data);
+  //show data in favorite tab
+  showFav(data);
 
-    //if addFav button is clicked
-    document.getElementById('addFav').addEventListener('click', (e) => {
+  //if addFav button is clicked
+  document.getElementById('addFav').addEventListener('click', (e) => {
 
-        var txt = '';
-        var regex = document.getElementById("enter-regex").value;//get regex value from user
-        var text = document.getElementById("enter-text").value;//get text to text value from user
-        var desc = ''
+      var txt = '';
+      var regex = document.getElementById("enter-regex").value;//get regex value from user
+      var text = document.getElementById("enter-text").value;//get text to text value from user
+      var desc = ''
 
-        var x;
-        var bool = false;
+      var x;
+      var bool = false;
 
-        for(x in data.regexArray){//for each item in regex array
-          if(regex === data.regexArray[x]){//check if there are duplicated totally similar regex
-              bool = true;//there is duplicated regex in localStorage
-             break;
-          }
+      for(x in data.regexArray){//for each item in regex array
+        if(regex === data.regexArray[x]){//check if there are duplicated totally similar regex
+            bool = true;//there is duplicated regex in localStorage
+           break;
         }
-        if(!bool){//if no duplicated regex, continue
+      }
+      if(!bool){//if no duplicated regex, continue
 
-          //show modal when addFav button clicked
-          $("#inputDescDetails").modal('show');
-          document.getElementById("enter-text").innerHTML=`Regex entered: ${regex}`//get regex value
-          //detect click to submit description
-          document.getElementById("descSubmit").addEventListener('click', (e) => {
-            desc=document.getElementById('enter-desc').value;
-            console.log(desc)
-            $("#inputDescDetails").modal('hide');
-          
-            //validate if description is empty
-            if (desc === null || desc === "") {
-              document.getElementById("favMessage").innerHTML = "Favorites adding insuccessful.";//show text and prompt user addign favorites insuccessful
-            }
-            else {
-
-
-              //add value into data object
-              data.regexArray.push(regex);
-              data.descArray.push(desc);
-              data.textArray.push(text)
-
-              //get current data and store into dateArray in data object
-              var d = new Date();
-              var datefullform = `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`;
-              data.dateArray.push(datefullform);
-
-              //store data into local storage
-              localStorage.setItem('favData', JSON.stringify(data));
-
-              document.getElementById("favMessage").innerHTML = `${regex} is successfully added to favorites.`;//set text for display message to user
-
-              showFav(data)
-              favorites(data);
-
-            }
-          });
-
-        }
-        else{
-          document.getElementById("favMessage").innerHTML = `${regex} exists in favorites.`;//set text for display message to user
-        }
+        //show modal when addFav button clicked
+        $("#inputDescDetails").modal('show');
+        document.getElementById("enter-text").innerHTML=`Regex entered: ${regex}`//get regex value
+        //detect click to submit description
+        document.getElementById("descSubmit").addEventListener('click', (e) => {
+          desc=document.getElementById('enter-desc').value;
+          console.log(desc)
+          $("#inputDescDetails").modal('hide');
         
-      })
+          //validate if description is empty
+          if (desc === null || desc === "") {
+            document.getElementById("favMessage").innerHTML = "Favorites adding insuccessful.";//show text and prompt user addign favorites insuccessful
+          }
+          else {
 
-      //if test button is clicked, hide favorite message
-      document.getElementById('test').addEventListener('click', (e) => {
-        document.getElementById("favMessage").innerHTML = ''
-      })
 
-      favorites(data);
+            //add value into data object
+            data.regexArray.push(regex);
+            data.descArray.push(desc);
+            data.textArray.push(text)
+
+            //get current data and store into dateArray in data object
+            var d = new Date();
+            var datefullform = `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`;
+            data.dateArray.push(datefullform);
+
+            //store data into local storage
+            localStorage.setItem('favData', JSON.stringify(data));
+
+            document.getElementById("favMessage").innerHTML = `${regex} is successfully added to favorites.`;//set text for display message to user
+
+            showFav(data)
+            favorites(data);
+
+          }
+        });
+
+      }
+      else{
+        document.getElementById("favMessage").innerHTML = `${regex} exists in favorites.`;//set text for display message to user
+      }
+      
+    })
+
+    //if test button is clicked, hide favorite message
+    document.getElementById('test').addEventListener('click', (e) => {
+      document.getElementById("favMessage").innerHTML = ''
+    })
+
+    favorites(data);
 
       
 
@@ -94,28 +94,49 @@ if (typeof(Storage) !== "undefined") {
 }
 
 //function to display modal and set value for elements in modal
+/** 
+   * @method  showFavModal
+   *
+   * @param  {Object}  data
+   * 
+   */
 function showFavModal(i, data){
 
-  let descInfo = document.getElementById('descContainer');
-  let textInfo = document.getElementById('textContainer');
+  let favInfo = document.getElementById('favInfoContainer');
 
 
   //Clear display area
-  descInfo.innerHTML = '';
-  let info1 = '';    
+  favInfo.innerHTML = '';
+  let info = '';    
+  
+  info+= `<form class="form-horizontal" id="viewDetailsForm" action="#">`
 
-  info1+= `<p>Date Added: ${data.dateArray[i]}</p>`
-  info1+= `<p>Description: ${data.descArray[i]}</p>`
+  info+= `<div class="form-group">`
+  info+= `<label class="control-label col-sm-3" for="regexDate">Date Added:</label>`
+  info+= `<div class="col-sm-8">`
+  info+= `<input type="text" class="form-control" id="regexDate" placeholder="Regex Title" name="regexDate" value="${data.dateArray[i]}" readonly>`
+  info+= `</div>`
+  info+= `</div>`
 
-  descInfo.innerHTML = info1;
+  info+= `<div class="form-group">`
+  info+= `<label class="control-label col-sm-3" for="regexDesc">Description: </label>`
+  info+= `<div class="col-sm-8">`
+  info+= `<input type="text" class="form-control" id="regexDesc" placeholder="Regex Text" name="regexDesc" value="${data.descArray[i]}" readonly>`
+  info+= `</div>`
+  info+= `</div>`
 
-  //Clear display area
-  textInfo.innerHTML = '';
-  let info2 = '';    
+  info+= `<div class="form-group">`
+  info+= `<label class="control-label col-sm-3" for="regexText">Text: </label>`
+  info+= `<div class="col-sm-8">`
+  info+= `<textarea id="regexText" placeholder="Regex Matches" name="regexText" readonly>`
+  info+= `${data.textArray[i]}`
+  info+= `</textarea>`
+  info+= `</div>`
+  info+= `</div>`
 
-  info2+= `<p>Text: ${data.textArray[i]}</p>`
+  info+= `</form>`
 
-  textInfo.innerHTML = info2;
+  favInfo.innerHTML = info;
 
   //after setting values, show modal to user
   $("#showFavDetails").modal('show');
@@ -123,61 +144,74 @@ function showFavModal(i, data){
 }
 
 //function to set on click listener for each element
+/** 
+   * @method  favorites
+   *
+   * @param  {Object}  data
+   * 
+   */
 function favorites(data){
   for (let i = data.regexArray.length-1; i >= 0; i -= 1) {
-      console.log('in favorites',i)
-        document.getElementById(`clickTrash${i}`).addEventListener('click', (e) => {
-          //alert(e.target.parentNode.id)
-          $("#deleteFav").modal('show');
+    console.log('in favorites',i)
 
-          //button in modal to trigger deletion
-          document.querySelector(".btn-success").addEventListener('click', (e) => {
-            data.regexArray.splice(i, 1)
-            data.descArray.splice(i, 1)
-            data.textArray.splice(i, 1)
-            data.dateArray.splice(i, 1)
+    document.getElementById(`clickTrash${i}`).addEventListener('click', (e) => {
+      //alert(e.target.parentNode.id)
+      $("#deleteFav").modal('show');
 
-            //update storage
-            localStorage.setItem('favData', JSON.stringify(data));
+      //button in modal to trigger deletion
+      document.querySelector(".btn-success").addEventListener('click', (e) => {
+        data.regexArray.splice(i, 1)
+        data.descArray.splice(i, 1)
+        data.textArray.splice(i, 1)
+        data.dateArray.splice(i, 1)
 
-            showFav(data);
-            favorites(data)
-            $("#deleteFav").modal('hide');
-            //console.log(`fav${i}, ${i}`)
-            return true;
-          })
+        //update storage
+        localStorage.setItem('favData', JSON.stringify(data));
 
-          //to avoid clicked on li and pop up modal
-          e.stopImmediatePropagation();
-          e.stopPropagation();
-          e.preventDefault();
+        showFav(data);
+        favorites(data)
+        $("#deleteFav").modal('hide');
+        //console.log(`fav${i}, ${i}`)
+        return true;
+      })
 
-        })
-        document.getElementById(`fav${i}`).addEventListener('click', (e) => {
-          //alert(`fav${i}, ${i}`)
-          showFavModal(i, data)
-        })
+      //to avoid clicked on li and pop up modal
+      e.stopImmediatePropagation();
+      e.stopPropagation();
+      e.preventDefault();
+
+    })
+    document.getElementById(`fav${i}`).addEventListener('click', (e) => {
+      //alert(`fav${i}, ${i}`)
+      showFavModal(i, data)
+    })
   }
 }
 
 //create and show li elements for each favorite regex
+/** 
+   * @method  showFav
+   *
+   * @param  {Object}  data
+   * 
+   */
 function showFav(data){
       
-      let list = document.getElementById('fav-ul-list');
+  let list = document.getElementById('fav-ul-list');
 
-      //Clear display area
-      list.innerHTML = '';
-      let info = '';    
+  //Clear display area
+  list.innerHTML = '';
+  let info = '';    
 
-      for (let i = data.regexArray.length-1; i >= 0; i -= 1) {
-        info+= `<li class="list-group-item favList" id="fav${i}">`
-        info+= `<span class="glyphicon glyphicon-trash pull-right" aria-hidden="true" id="clickTrash${i}"></span>`
-        info+= `<h5 class="list-title ">${data.regexArray[i]}</h5>`
-        info+= `<h6 class="list-date">${data.dateArray[i]}</h6>`
-        info+= `</li>`
-      }
+  for (let i = data.regexArray.length-1; i >= 0; i -= 1) {
+    info+= `<li class="list-group-item favList" id="fav${i}">`
+    info+= `<span class="glyphicon glyphicon-trash pull-right" aria-hidden="true" id="clickTrash${i}"></span>`
+    info+= `<h5 class="list-title ">${data.regexArray[i]}</h5>`
+    info+= `<h6 class="list-date">${data.dateArray[i]}</h6>`
+    info+= `</li>`
+  }
 
-      list.innerHTML = info;
+  list.innerHTML = info;
 }
 
 
